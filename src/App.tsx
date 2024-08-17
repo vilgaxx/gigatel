e42715b29cc59e489348599529d31ee958be2036
vilgaxx <codervilgax@gmail.com>
2024-08-18 03:48:57 +0530

    logo update

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => {
               {/* <Package2 className="h-6 w-6" /> */}
               <img
                 className="h-10 w-auto"
-                src="src\assets\1transparent.png"
+                src="src\assets\transparent2.png"
                 alt="GigaZone Logo"
               />
               <span className="sr-only">WifiWala</span>
